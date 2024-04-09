5b289039008afe831cc5b830a6eb8b0b121cd34f
Humayra <humayra289@gmail.com>
2024-04-09 14:28:36 -0400

    signIn implement

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -9,128 +10,156 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+
+  //text controllers
+
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  Future signIn() async{
+    await FirebaseAuth.instance.signInWithEmailAndPassword(
+      email: _emailController.text.trim(),
+      password: _passwordController.text.trim(),
+    );
+  }
+
+  @override
+  void dispose(){
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.deepPurple[100],
       body: SafeArea(
         child: Center(
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(
-                  Icons.medical_information,
-                  size: 100,
-                ),
-                SizedBox(height: 75),
-                //Welcome back!
-                Text(
-                  'Hello Again!',
-                  style: GoogleFonts.bebasNeue(
-                    fontSize: 52,
+          child: SingleChildScrollView(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.medical_information,
+                    size: 100,
                   ),
-                ),
-                SizedBox(height: 10),
-                Text(
-                  'Welcome back, you\'ve been missed!',
-                  style: TextStyle(
-                    fontSize: 20,
-                  ),
-                ),
-          
-                SizedBox(height: 50),
-
-                //email textfield
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.deepPurple[50],
-                      border: Border.all(color: Colors.white),
-                      borderRadius: BorderRadius.circular(12),
+                  SizedBox(height: 75),
+                  //Welcome back!
+                  Text(
+                    'Hello Again!',
+                    style: GoogleFonts.bebasNeue(
+                      fontSize: 52,
                     ),
-                    child: Padding(
-                      padding: const EdgeInsets.only(left: 20.0),
-                      child: TextField(
-                        decoration: InputDecoration(
-                          border: InputBorder.none,
-                          hintText: 'Email',
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    'Welcome back, you\'ve been missed!',
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+            
+                  SizedBox(height: 50),
+            
+                  //email textfield
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.deepPurple[50],
+                        border: Border.all(color: Colors.white),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 20.0),
+                        child: TextField(
+                          controller: _emailController,
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: 'Email',
+                            filled: true,
+                          ),
                         ),
                       ),
                     ),
                   ),
-                ),
-
-                SizedBox(height: 10),
-
-                //password textfield
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.deepPurple[50],
-                      border: Border.all(color: Colors.white),
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.only(left: 20.0),
-                      child: TextField(
-                        obscureText: true, //makes the password obscure
-                        decoration: InputDecoration(
-                          border: InputBorder.none,
-                          hintText: 'Password',
+            
+                  SizedBox(height: 10),
+            
+                  //password textfield
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.deepPurple[50],
+                        border: Border.all(color: Colors.white),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 20.0),
+                        child: TextField(
+                          controller: _passwordController,
+                          obscureText: true, //makes the password obscure
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: 'Password',
+                          ),
                         ),
                       ),
                     ),
                   ),
-                ),
-          
-                SizedBox(height: 10),
-
-                //signin button
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                  child: Container(
-                    padding: EdgeInsets.all(20),
-                    decoration: BoxDecoration(
-                      color: Colors.deepPurple,
-                      borderRadius: BorderRadius.circular(12)
+            
+                  SizedBox(height: 10),
+            
+                  //signin button
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                    child: GestureDetector(
+                      onTap: signIn,
+                      child: Container(
+                        padding: EdgeInsets.all(20),
+                        decoration: BoxDecoration(
+                          color: Colors.deepPurple,
+                          borderRadius: BorderRadius.circular(12)
+                        ),
+                        child: Center(
+                          child: Text(
+                            'Sign In',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                            ),
+                          )
+                        ),
+                      ),
                     ),
-                    child: Center(
-                      child: Text(
-                        'Sign In',
+                  ),
+            
+                  SizedBox(height: 25),
+            
+                  //not a member? register now
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Not a member?', 
                         style: TextStyle(
-                          color: Colors.white,
                           fontWeight: FontWeight.bold,
-                          fontSize: 18,
-                        ),
-                      )
-                    ),
+                        )
+                      ),
+                      Text(
+                        ' Register now', 
+                        style: TextStyle(
+                          color: Colors.blue, 
+                          fontWeight: FontWeight.bold,
+                        )
+                      ),
+                    ],
                   ),
-                ),
-          
-                SizedBox(height: 25),
-
-                //not a member? register now
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Not a member?', 
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                      )
-                    ),
-                    Text(
-                      ' Register now', 
-                      style: TextStyle(
-                        color: Colors.blue, 
-                        fontWeight: FontWeight.bold,
-                      )
-                    ),
-                  ],
-                ),
-            ],
+              ],
+            ),
           ),
         ), 
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:form_demo/firebase_options.dart';
-import 'package:form_demo/forms_screen.dart';
-import 'login_page.dart';
+//import 'package:form_demo/forms_screen.dart';
+import 'package:form_demo/main_page.dart';
+//import 'login_page.dart';
 
 void main() async{
   //async lets us continuously access from our project to our firebase backend
   WidgetsFlutterBinding.ensureInitialized();
+  //await Firebase.initializeApp();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
@@ -19,7 +21,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.cyan),
-      home: LoginPage(),
+      home: MainPage(),
       //home: FormsScreen(),
     );
   }
